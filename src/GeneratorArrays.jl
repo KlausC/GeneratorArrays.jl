4cc44f9cae76052ba19fd3b23ba9184573528849
Klaus Crusius <klaus.crusius@web.de>
2020-05-08 21:59:41 +0200

    added tests and fixed productiterator

--- src/GeneratorArrays.jl
+++ src/GeneratorArrays.jl
@@ -1,11 +1,11 @@
 module GeneratorArrays
 
-export array, GeneratorArray
+export array
 
 import Base.Generator
-import Base: IteratorSize, HasShape, HasLength, length, axes
+import Base: IteratorSize, HasShape, HasLength, length, axes, size, iterate
 import Base: IteratorEltype, eltype
-import Base: getindex, iterate
+import Base: getindex
 import Base.Iterators: ProductIterator
 
 struct GeneratorArray{T,N,G} <: AbstractArray{T,N}
@@ -13,35 +13,63 @@ struct GeneratorArray{T,N,G} <: AbstractArray{T,N}
 end
 
 array(a::AbstractArray) = a
+array(gen::G) where G = _array(G)(gen)
 
-function array(gen::G) where G
-    array(G)(gen)
+function getindex(a::GeneratorArray{T,N,<:Generator}, i::Integer...) where {T,N}
+    it = a.gen.iter
+    i = adjust_indices(N, i, a)
+    a.gen.f(array(it)[i...])::T
 end
 
-array(::Type{G}) where G = array(G, IteratorSize(G))
-
-function array(::Type{G}, ::HasShape) where G
-    T = _eltype(G)
-    N = dimension(G)
-    GeneratorArray{T,N,G}
+function getindex(a::GeneratorArray{T,N,<:ProductIterator}, i::Integer...) where {T,N}
+    it = a.gen.iterators
+    n = length(it)
+    i = adjust_indices(N, i, it)
+    dim = 0
+    res = Vector{Any}(undef, n)
+    for k = 1:n
+        sit = it[k]
+        di = dimension(sit) + dim
+        si = i[dim+1:di]
+        dim = di
+        res[k] = getindex(array(sit), si...)
+        #println(sit, "[", si..., "] = ", res[k])
+    end
+    tuple(res...)
 end
+
+iterate(a::GeneratorArray, s...) = iterate(a.gen, s...)
 
 IteratorSize(::Type{<:GeneratorArray{T,N,G}}) where {T,N,G} = IteratorSize(G)
 length(a::GeneratorArray) = length(a.gen)
 axes(a::GeneratorArray) = axes(a.gen)
+size(a::GeneratorArray) = size(a.gen)
 
 IteratorEltype(::Type{<:GeneratorArray{T,N,G}}) where {T,N,G} = IteratorEltype(G)
 eltype(a::GeneratorArray{T}) where T = T
 
+# - implementation details
+
+_array(::Type{G}) where G = _array(G, IteratorSize(G))
+function _array(::Type{G}, ::HasShape) where G
+    T = _eltype(G)
+    N = dimension(G)
+    GeneratorArray{T,N,G}
+end
+_array(T::Type, S::IteratorSize) = throw(DomainError(S, "unsupported iterator type $T"))
+
 function _eltype(gen::Type{<:Generator{B,F}}) where {B,F}
-    E = eltype(array(B))
+    E = eltype(_array(B))
     if F === typeof(identity)
         E
     else
         promote_type(Base.return_types(F.instance, (E,))...)
     end
 end
-
+_eltype(::Type{<:ProductIterator{T}}) where T = _eltype(T)
+_eltype(::Type{Tuple{I}}) where I = _eltype(I)
+_eltype(::Type{Tuple{I,J}}) where {I,J} = Tuple{_eltype(I),_eltype(J)}
+_eltype(::Type{Tuple{I,J,K}}) where {I,J,K} = Tuple{_eltype(I),_eltype(J),_eltype(K)}
 _eltype(itr) = eltype(itr)
 
 dimension(a::T) where T = dimension(T)
@@ -49,23 +77,10 @@ dimension(::Type{G}) where G = dimension(IteratorSize(G))
 dimension(::IteratorSize) = 1
 dimension(::HasShape{N}) where N = N
 
-function getindex(a::GeneratorArray{T,N,<:Generator}, i::Integer...) where {T,N}
-    it = a.gen.iter
-    i = adjust_indices(N, i)
-    a.gen.f(array(it)[i...])::T
+function adjust_indices(n::Integer, i, it)
+    m = length(i)
+    (m == 0 || m > n && any(i[n+1:m] .!= 1)) && throw(BoundsError(it, i))  
+    m == n ? i : m < n ? (i..., ones(Int, n-m)...) : i[1:n]
 end
-
-function getindex(a::GeneratorArray{T,N,<:ProductIterator}, i::Integer...) where {T,N}
-    it = a.gen.iterators
-    i = adjust_indices(length(it), i)
-    getindex.(it, i)::T
-end
-
-function adjust_indices(n::Integer, i)
-    m = n - length(i)
-    m == 0 ? i : m > 0 ? (i..., ones(Int, m)...) : i[1:n]
-end
-
-iterate(a::GeneratorArray, s...) = iterate(a.gen, s...)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,66 @@
 using GeneratorArrays
 using Test
 
-@testset "GeneratorArrays.jl" begin
-    # Write your own tests here.
+@testset "GeneratorArrays" begin
+
+    @testset "iterators" begin
+        let i0 = 1:0.5:5, i1 = [1;2;3]; i2 = [Float16(1) 2; 3 4; 5 6]
+        @testset "arrays $it" for (it,T,N) in ((i0,Float64,1),
+                                               (i1,Int,1),
+                                               (i2,Float16,2))
+            @test array(it) === it
+            gen = (x for x in it)
+            @test length(axes(array(gen))) == N
+            agen = array(gen)
+            @test axes(agen) == axes(it) 
+            @test size(agen) == size(it) 
+            @test length(agen) == length(it)
+            @test iterate(agen) == iterate(it)
+            @test agen == collect(it)
+            @test agen[3] == it[3]
+        end
+
+        @test_throws DomainError array("abc") 
+        end
+    end
+
+    @testset "generators" begin
+        a = [1.0; 2; 3]
+        let g1 = (Float16(sin(x)) for x in 1:5), g3 = (x*y for x in a, y in 1:5, z = 1:2)
+        @testset "generator dim $N" for (it,T,N,s) in ((g1,Float16,1,(5,)),
+                                                    (g3,Float64,3,(3,5,2)))
+            @test array(it).gen === it
+            agen = array(it)
+            @test length(axes(agen)) == N
+            @test size(agen) == s
+            @test length(agen) == prod(s)
+            @test collect(agen) == collect(it)
+            @test agen == collect(it)
+        end
+        agen = array(g1)
+        @test agen[3] == Float16(sin(3.0))
+        agen = array(g3)
+        @test agen[1] == 1
+        @test agen[1,2] == 2
+        @test agen[1,2,2] == 2
+        @test_throws BoundsError agen[1,2,3]
+        @test agen[1,2,1,1] == 2
+        @test_throws BoundsError agen[]
+        @test_throws BoundsError agen[1,1,1,17]
+
+        @test_throws DomainError array((x*y for x in a for y in a))
+        end
+    end
+
+    @testset "recursive" begin
+        let g0 = (x^2 for x in -2:2), g1 = (x*y for x in 1:2, y in 0:10)
+            
+            g2 = ((s, t) for s in g0, t in g1)
+            a2 = array(g2)
+            @test size(a2) == (5,2,11)
+            @test eltype(a2) == Tuple{Int,Int}
+            @test a2 == collect(g2)
+        end
+    end 
 end
+
